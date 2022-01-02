464bf2a0696918c6c1f5101370b0e25ae4a07c4d
ariel-gi <arielwarshawsky@gmail.com>
2022-01-01 23:53:50 -0800

    make it better

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,6 +13,20 @@ export const getPosts = async (req, res) => {
   }
 };
 
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery } = req.query;
+  try {
+    const teacher = new RegExp(searchQuery, "i");
+
+    const posts = await PostHomework.find({ teacher: teacher });
+    console.log(posts);
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 export const createPost = async (req, res) => {
   const post = req.body;
 
